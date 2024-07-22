febc9b97166978d4cac098df8ae77fb3da26f8a8
Zhao Luo <zhao.luo@student.hogent.be>
2024-07-22 15:31:31 +0200

    Fix

--- bachelorproefvuescanner.client/vite.config.js
+++ bachelorproefvuescanner.client/vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import plugin from '@vitejs/plugin-vue';
 import fs from 'fs';
 import path from 'path';
 import child_process from 'child_process';
@@ -35,7 +35,7 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [plugin()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
